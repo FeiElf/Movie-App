eeb1172f44fc59d5fb94fee31c767f74cb4476bb
Yifei Dong <yifeid3@gmail.com>
2023-11-22 14:15:09 +0930

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by USM Admin on 22/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
